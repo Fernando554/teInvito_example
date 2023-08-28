08869fccd3e2b3aca4036c48ce90c0d44eecb3c8
tlaxcalteco <pedro_1320114068@uptecamac.edu.mx>
2023-08-28 16:19:01 -0600

    slide

--- resources/views/livewire/slide-section.blade.php
+++ resources/views/livewire/slide-section.blade.php
@@ -5,33 +5,10 @@
         <div class="carousel-item active">
             <img src="https://elcomercio.pe/resizer/gj5JbwxkmqRAa4HSpfOHEIUBf7k=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/6FUBT6XQXNHHNFOMCHIT7I34NA.jpg" class="d-block w-100" alt="Imagen 1">
             <div class="carousel-caption">
-                <h1 class="display-1">Título</h1>
-                <p class="lead">Contenido</p>
-                <small>Subcontenido</small>
-                <br>
-                <a href="#" class="btn btn-primary btn-lg">Botón</a>
-            </div>
-        </div>
-        <!-- Segunda diapositiva -->
-        <div class="carousel-item">
-            <img src="https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2023/06/transformers-rise-7680x4320-11402-scaled.jpg?fit=2560%2C1440&quality=50&strip=all&ssl=1" class="d-block w-100" alt="Imagen 2">
-            <div class="carousel-caption">
-                <h1 class="display-1">Otro Título</h1>
-                <p class="lead">Otro Contenido</p>
-                <small>Otro Subcontenido</small>
-                <br>
-                <a href="#" class="btn btn-primary btn-lg">Otro Botón</a>
-            </div>
-        </div>
-
-        <div class="carousel-item">
-            <img src="https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2023/06/transformers-rise-7680x4320-11402-scaled.jpg?fit=2560%2C1440&quality=50&strip=all&ssl=1" class="d-block w-100" alt="Imagen 2">
-            <div class="carousel-caption">
-                <h1 class="display-1">Otro Título</h1>
-                <p class="lead">Otro Contenido</p>
-                <small>Otro Subcontenido</small>
-                <br>
-                <a href="#" class="btn btn-primary btn-lg">Otro Botón</a>
+               <input wire:model="title" class="form-control">
+                <textarea wire:model="content" class="form-control"></textarea>
+                <textarea wire:model="subcontent" class="form-control"></textarea>
+                <button wire:click="saveChanges" class="btn btn-primary">Guardar</button>
             </div>
         </div>
         <!-- Agrega más diapositivas según sea necesario -->

c17d97dc715b9af165acbfeb5d038d7b5f2502de
Fernando554 <fernando_1320114093@uptecamac.edu.mx>
2023-09-14 16:03:37 -0600

    preview

--- resources/views/livewire/header-section.blade.php
+++ resources/views/livewire/header-section.blade.php
@@ -1,10 +1,11 @@
 <div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh;">
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-6 mb-3">
             <div class="card">
                 <div class="card-header">
                     @if ($isEditing)
                         <input wire:model="title" class="form-control">
+                        <label for="title">{{ $title }}</label>
                     @else
                         {{ $title }}
                     @endif
@@ -26,16 +27,31 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-6">
             @if ($isEditing)
                 <input type="file" wire:model="Image">
             @else
             <img src="{{asset('/storage/app/public/' . $Image)}}" alt="Imagen" class="img-fluid" style="max-width: 400px; height: 400px;">
             @endif
         </div>
+        @if ($isEditing)
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    {{ $title }}
+                </div>
+                <div class="card-body text-center text-wrap">
+                        {!! $body !!}
+                        <a href="{{$link}}" class="btn btn-primary">{{ $linkText }}</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <img src="{{asset('/storage/app/public/' . $Image)}}" alt="Imagen" class="img-fluid" style="max-width: 400px; height: 400px;">
+        </div>
+        @endif
     </div>
 </div>
-
 <script>
     $(document).ready(function() {
         $('#body').summernote({
